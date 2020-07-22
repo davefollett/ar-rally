60f7eefe573d28eed362ee04b2956435114b07ef
Dave Follett <davefollett@gmail.com>
2020-07-21 22:55:41 -0400

    attempting to add ar,js inside  vue app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import LoadScript from 'vue-plugin-load-script'
+
+Vue.use(LoadScript)
 
 Vue.config.productionTip = false
+
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-entity',
+  'a-camera',
+  'a-box',
+  'a-text'
+]
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-scene v-if="scriptsLoaded"
+      vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+    >
+      <a-text
+        value="Gazebo"
+        look-at="[gps-camera]"
+        scale="120 120 120"
+        gps-entity-place="latitude: 42.8262959; longitude: -83.2889924;"
+      ></a-text>
+      <a-camera gps-camera="maxDistance: 500" rotation-reader > </a-camera>
+    </a-scene>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    scriptsLoaded: false
+  }),
+  mounted () {
+    // document.body.appendChild(this.$el)
+
+    Promise.all([
+      this.$loadScript('https://aframe.io/releases/1.0.4/aframe.min.js'),
+      this.$loadScript('https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js'),
+      this.$loadScript('https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js')
+    ]).then(() => {
+      console.log('success')
+      this.scriptsLoaded = true
+    }).catch(() => {
+      console.log('error')
+      this.scriptsLoaded = false
+    })
+
+    // this.$loadScript('https://aframe.io/releases/1.0.4/aframe.min.js')
+    //   .then(() => {
+    //     console.log('success')
+    //   })
+    //   .catch(() => {
+    //     console.log('error')
+    //   })
   }
 }
 </script>
